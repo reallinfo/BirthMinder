f4567d6da9feb81e22db43c3c9061390e5b41097
C. Suarez <admync@gmail.com>
2019-05-01 23:13:14 +0200

    generación de la vista(template) y enlace con views

--- reminder/views.py
+++ reminder/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Persona
 # Create your views here.
+def index(request):#la funcion que llamamos desde el mapeador url de reminder
+    """
+    Función para generar la vista del index
+    """
+    #Obtencion del numero de registros (personas)
+    num_personas = Persona.objects.all().count()
+    
+    
+    #Hacemos el render de la lantilla html añadiendo los datos mediante la variable context que es un diccionario que contiene los datos con su marcador
+    return render(request,'index.html',
+                  context = {'num_personas':num_personas,}
+                  )
