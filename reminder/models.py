010527258fce27f55e9b6dfa0b28925e6f775180
C. Suarez <admync@gmail.com>
2019-05-01 22:10:42 +0200

    migración del modelo

--- reminder/models.py
+++ reminder/models.py
@@ -1,26 +1,25 @@
 from django.db import models
-
+from django.urls import reverse #para generar las url
 # Create your models here.
+
 class Persona(models.Model):
     """
-    Una clase típica definiendo un modelo, derivado desde la clase Model.
+    Una clase definiendo un modelo ( Persona ), derivado desde la clase Model.
     """
-
-    # Campos
     nombre = models.CharField(max_length=20, help_text="Nombre de la persona")
     apellidos = models.CharField(max_length=20, help_text="Apellidos de la persona")
-	edad = models.PositiveIntegerField(max_length=20, help_text="Edad de la persona")
-	fecha = models.DateField(max_length=20, help_text="Dia y mes del cumpleaños")
+    edad = models.PositiveIntegerField(max_length=20, help_text="Edad de la persona")
+    fecha = models.DateField(max_length=20, help_text="Dia y mes del cumpleaños")
     # Metadata
     class Meta: 
-        ordering = ["nombre"]
+    	ordering = ["nombre"]
 
     # Métodos
     def get_absolute_url(self):
-         """
-         Devuelve la url para acceder a una instancia particular de MyModelName.
-         """
-         return reverse('model-detail-view', args=[str(self.id)])
+        """
+        Devuelve la url para acceder a una instancia particular de Persona.
+        """
+        return reverse('model-detail-view', args=[str(self.id)])
     
     #Obligatoria!
     def __str__(self):
