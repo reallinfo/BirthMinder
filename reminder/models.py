6c429b3ebc224822204804b98d54e4295572987d
C. Suarez <admync@gmail.com>
2019-05-01 18:38:56 +0200

    creacion del modelo base para Persona

--- reminder/models.py
+++ reminder/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    """
+    Una clase típica definiendo un modelo, derivado desde la clase Model.
+    """
+
+    # Campos
+    nombre = models.CharField(max_length=20, help_text="Nombre de la persona")
+    apellidos = models.CharField(max_length=20, help_text="Apellidos de la persona")
+	edad = models.PositiveIntegerField(max_length=20, help_text="Edad de la persona")
+	fecha = models.DateField(max_length=20, help_text="Dia y mes del cumpleaños")
+    # Metadata
+    class Meta: 
+        ordering = ["nombre"]
+
+    # Métodos
+    def get_absolute_url(self):
+         """
+         Devuelve la url para acceder a una instancia particular de MyModelName.
+         """
+         return reverse('model-detail-view', args=[str(self.id)])
+    
+    #Obligatoria!
+    def __str__(self):
+        """
+        Cadena para representar el objeto Persona (en el sitio de Admin, etc.)
+        """
+        return self.nombre
+        return self.apellidos
+        return self.edad
+        return self.fecha
